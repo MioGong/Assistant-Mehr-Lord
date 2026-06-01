851bd8e159aa1e6907fc50cea8995792a8f66860
mehrzad07 <Mehrzadshahsavari07@gmail.com>
2026-06-01 14:32:48 +0330

    Update bot 2

--- myBot.py
+++ myBot.py
@@ -213,7 +213,7 @@ def info(message):
 
 @bot.message_handler(commands=['pair'])
 def pair(message):
-    bot.reply_to(message, "❤️ عشق مهرزاد همیشه غزله ❤️")
+    bot.reply_to(message, "❤️ مهرزاد زتو رو خیلی دوست داره ❤️")
 
 @bot.message_handler(commands=['date'])
 def send_date(message):
